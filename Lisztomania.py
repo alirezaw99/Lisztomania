9d4b65ed53f1d0384c1dc75b76829d614aa7ff53
Alien <128927380+alirezaw99@users.noreply.github.com>
2023-09-11 23:00:40 +0330

    bot created
    lisztomania bot created and 'start' function is defined

--- Lisztomania.py
+++ Lisztomania.py
@@ -5,7 +5,7 @@ async def start(update : Update, context : ContextTypes.DEFAULT_TYPE) -> None :
     await update.message.reply_text(f'Hello {update.effective_user.first_name} Wellcome to Lisztomania') 
     # context.bot.send_message(chat_id=update.effective_chat.id, text='Hello! I\'m Lisztomania. \n You can convert your music to voice message with me. \n And don\'t forget to Stay @Biskiviti ')
 
-app = ApplicationBuilder().token('6571724046:AAGqsTLfv2qE4EOZyj5WrMBLH2V35ABxZJo').build()
+app = ApplicationBuilder().token('Token').build()
 start_handler = CommandHandler('start', start)
 app.add_handler(start_handler)
 
